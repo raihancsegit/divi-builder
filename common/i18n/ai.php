ec613b6f45fc6a5dbf6587a9302dcafa1b3a4ef8
Raihan Islam <raihanislam.cse@gmail.com>
2024-04-20 09:55:36 +0600

    update

---
+++ common/i18n/ai.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Internationalization.
+ *
+ * @package Divi
+ */
+
+return [
+	'Search'          => esc_html__( 'Search', 'et_builder' ),
+	'Translate'       => esc_html__( 'Translate', 'et_builder' ),
+	'Quick Actions'   => esc_html__( 'Quick Actions', 'et_builder' ),
+];
